7cf7ae4f1bde501247503ee1f8978331e9ba8f71
George Thayamkery <george.thayamkery@gmail.com>
2021-10-14 03:42:24 -0500

    updates to build

--- libs/fetcher.ts
+++ libs/fetcher.ts
@@ -1,7 +1,6 @@
 // Need a package that can dynamically switch between the JS `fetch`
 // and the `node-fetch` based on usage.
 import fetch from 'isomorphic-unfetch';
-import { VercelRequest } from '@vercel/node';
 
 export async function fetcher<JSON = unknown>(input: RequestInfo, init?: RequestInit): Promise<JSON> {
   const res = await fetch(input, {
@@ -14,18 +13,18 @@ export async function fetcher<JSON = unknown>(input: RequestInfo, init?: Request
   return res.json();
 }
 
-export const authenticatedFetch = <JSON = unknown>(input: RequestInfo, req: VercelRequest, init?: RequestInit): Promise<JSON> => {
-  const { accessToken } = req.cookies;
-  return fetcher(input, {
-    ...init,
-    headers: {
-      ...init?.headers,
-      Cookie: `accessToken=${accessToken}`
-    }
-  });
-};
+// export const authenticatedFetch = <JSON = unknown>(input: RequestInfo, req: VercelRequest, init?: RequestInit): Promise<JSON> => {
+//   const { accessToken } = req.cookies;
+//   return fetcher(input, {
+//     ...init,
+//     headers: {
+//       ...init?.headers,
+//       Cookie: `accessToken=${accessToken}`
+//     }
+//   });
+// };
 
-export const getBaseUrl = (req: VercelRequest): string => {
-  const httpProto = req.headers['x-forwarded-proto'] || 'https';
-  return `${httpProto}://${req.headers.host}`;
-};
+// export const getBaseUrl = (req: VercelRequest): string => {
+//   const httpProto = req.headers['x-forwarded-proto'] || 'https';
+//   return `${httpProto}://${req.headers.host}`;
+// };
